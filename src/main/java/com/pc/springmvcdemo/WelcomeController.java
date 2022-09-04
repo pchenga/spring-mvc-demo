c748ac4756ffdee2887fc6370d5088946428f5fc
Prakash Chenga <PChenga83@gmail.com>
2022-09-04 11:45:42 +0530

    Added

---
+++ src/main/java/com/pc/springmvcdemo/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.pc.springmvcdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring Maven App";
+    }
+}
